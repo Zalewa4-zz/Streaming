e750bacc4d5e50a162bbf0bbd5304bc5399ec40a
Zalewa4 <zalewa4444@gmail.com>
2021-02-22 22:58:46 +0100

    d

--- loggeduser/paymentscript.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'header.php';
-    $login=$_SESSION['login'];
-    $zaplata=$db->query('UPDATE users SET ispaid = ? WHERE login =?', 1, $login);
-    alert("Dokonałeś wpłaty!", "Od teraz możesz oglądać filmy i seriale!", "success", "Streaming Zalewki", "payup.php");        
-require('footer.php');
-
-?>
